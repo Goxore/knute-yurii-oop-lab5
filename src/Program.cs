347a497aaedb4814826365f3bf644cb876004595
Goxore <ggooxxoorree@gmail.com>
2022-10-22 21:42:13 +0200

    fix

--- src/Program.cs
+++ src/Program.cs
@@ -3,10 +3,10 @@
 
     public static void Main(string[] args)
     {
-        // System.Console.WriteLine("---------------------------");
-        // Exercise1.Run();
-        // System.Console.WriteLine("---------------------------");
-        // Exercise2.Run();
+        System.Console.WriteLine("---------------------------");
+        Exercise1.Run();
+        System.Console.WriteLine("---------------------------");
+        Exercise2.Run();
         System.Console.WriteLine("---------------------------");
         Exercise3.Run();
     }
